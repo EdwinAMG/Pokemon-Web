f6f4725600e84446e83085666a2c74eb1ecdba6c
Edwin M <andresedwinmg14@gmail.com>
2023-02-22 08:54:39 -0500

    Primer commit

---
+++ src/Components/Main.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+const Main=()=>{
+    return(
+        <>
+            <div className="container">
+                <div className="left-content">   /*Se separa cada parte de la página 
+
+
+                </div>
+                <div className="right-content">
+
+
+                </div>
+
+            </div>
+
+         </>
+    )
+}
+export default Main;
